eebadb1a39fe27a657c3408b6d12a0c483c3ade6
Mohammed Imran <imran@factoreal.com>
2023-11-29 22:09:08 +0530

    Add JWT_SECRET and BLACKLIST_ID environment variables

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"strconv"
 	"strings"
 
 	"github.com/aws/aws-lambda-go/events"
@@ -13,21 +15,39 @@ import (
 )
 
 func handler(ctx context.Context, event events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	var AuthHeader = event.Headers["authorization"]
+	jwtSecret := os.Getenv("JWT_SECRET")
+	if jwtSecret == "" {
+		log.Println("JWT_SECRET is not set")
+		jwtSecret = "secret"
+	}
+	BLACKLIST_ID := os.Getenv("BLACKLIST_ID")
+	if BLACKLIST_ID == "" {
+		log.Println("BLACKLIST_ID is not set")
+		BLACKLIST_ID = "1234"
+	}
 
+	var AuthHeader = event.Headers["authorization"]
 	if AuthHeader == "" {
 		log.Println("Could not get the token from the header")
-
 		return events.APIGatewayProxyResponse{StatusCode: http.StatusUnauthorized, Body: "Unauthorized"}, nil
 	}
+
 	AuthHeader = strings.Replace(AuthHeader, "Bearer ", "", 1)
-	jwt, err := utils.DecodeJwtClaim(AuthHeader)
+	claims, err := utils.VerifyJWT(AuthHeader, jwtSecret)
 	if err != nil {
 		log.Println("Could not decode the JWT", err.Error())
 		return events.APIGatewayProxyResponse{StatusCode: http.StatusUnauthorized, Body: "Unauthorized"}, nil
 	}
 
-	return events.APIGatewayProxyResponse{StatusCode: http.StatusOK, Body: fmt.Sprintf("Valid Enterprize: %d", jwt.Id)}, nil
+	var responseHeaders = map[string]string{
+		"Location": "https://www.google.com",
+	}
+
+	if strconv.Itoa(claims.UserId) == BLACKLIST_ID {
+		return events.APIGatewayProxyResponse{StatusCode: http.StatusMovedPermanently, Headers: responseHeaders}, nil
+	}
+
+	return events.APIGatewayProxyResponse{StatusCode: http.StatusOK, Body: fmt.Sprintf("Valid Enterprize: %d", claims.UserId)}, nil
 }
 
 func main() {
